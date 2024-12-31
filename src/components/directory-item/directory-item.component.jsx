58ce90e5778d78d85f41db92939cebf0b9e3970e
touhidzaman007 <79441219+touhidzaman007@users.noreply.github.com>
2025-01-01 00:46:59 +0600

    Implement navigation for directory items; add route handling in DirectoryItem and update Directory component to use static categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import {
   BackgroundImage,
   Body,
@@ -5,9 +7,15 @@ import {
 } from './directory-item.styles';
 
 function DirectoryItem({ category }) {
-  const { id, title, imageUrl } = category;
+  const { id, title, imageUrl, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(route);
+  };
+
   return (
-    <DirectoryItemContainer key={id}>
+    <DirectoryItemContainer key={id} onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
