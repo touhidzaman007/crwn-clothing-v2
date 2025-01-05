2965528c9852597bf28e3e6fdfb3216152503ab5
touhidzaman007 <79441219+touhidzaman007@users.noreply.github.com>
2025-01-06 05:48:19 +0600

    updated firebase config

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,7 +20,6 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
-
 const firebaseConfig = {
   apiKey: 'AIzaSyDrcRvi_5Ru1qg2enqzjJMwlrWNphu3qlM',
   authDomain: 'crown-clothing-c5a19.firebaseapp.com',
